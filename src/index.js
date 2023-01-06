7efe749a86f167bcb8f42ae1097ceaf205cdffb1
Liudmila <yulyutayoo@gmail.com>
2023-01-07 04:10:54 +0900

    feat: add new codebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    a = n.toString();
+    c = a.length;
+    let i;
+    let result='';
+    for (i=c-1; i>=0; i--)  {
+        result += a[i];
+    }
+    return result;
 }
